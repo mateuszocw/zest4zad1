71b93f03fc21b1a953df57ad23bc6820aaf1a8b7
mateuszocw <57373230+mateuszocw@users.noreply.github.com>
2019-11-04 22:32:32 +0100

    Update rozwiazanie.cpp

--- rozwiazanie.cpp
+++ rozwiazanie.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 using namespace std;
-const int m = 16;
+const int MAX = 16;
 
 int main() {
-	int t[m][m] = {};
-	int ldw = m * m; //liczba do wpisania
-	int irw = m; //ile razy wpisac
+	int t[MAX][MAX] = {};
+	int ldw = MAX * MAX; //liczba do wpisania
+	int irw = MAX; //ile razy wpisac
 	int p = 0; //przypadek (1z4)
 	int i = 0, j = 0;
 	while (ldw > 0 && irw > 0) {
@@ -17,40 +17,34 @@ int main() {
 				t[i][j] = ldw;
 				ldw--;
 			}
-			j--;
-			i++;
-			irw--;
+			j--;i++;irw--;
 			break;
 		case 2: //w dół
 			for (int k = 0; k < irw; i++, k++) {
 				t[i][j] = ldw;
 				ldw--;
 			}
-			i--;
-			j--;
+			i--;j--;
 			break;
 		case 3: //w lewo
 			for (int k = 0; k < irw; j--, k++) {
 				t[i][j] = ldw;
 				ldw--;
 			}
-			j++;
-			i--;
-			irw--;
+			j++;i--;irw--;
 			break;
 		case 0: //w górę
 			for (int k = 0; k < irw; i--, k++) {
 				t[i][j] = ldw;
 				ldw--;
 			}
-			i++;
-			j++;
+			i++;j++;
 			break;
 		}
 	}
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < MAX; i++) {
 		cout << "\n\n\n";
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < MAX; j++) {
 			cout << "\t" << t[i][j];
 		}
 	}
